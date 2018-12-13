9b6c9ab82b4637f599e3e7a7522bfcac1fe65ba3
Danilo Pimentel <danilo.pimentel97@gmail.com>
2018-12-13 08:26:53 -0200

    Adiciona validação para nome, cpf e telefone no cadastro de cliente.

--- src/clinicasaracura/views/CadastroClienteView.java
+++ src/clinicasaracura/views/CadastroClienteView.java
@@ -7,6 +7,7 @@ package clinicasaracura.views;
 
 import clinicasaracura.controllers.ClientesController;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import static java.awt.Component.CENTER_ALIGNMENT;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -27,7 +28,9 @@ public class CadastroClienteView extends JPanel {
     JTextField nomeField;
     JTextField cpfField;
     JTextField telefoneField;
-    
+    JLabel cpfError;
+    JLabel telefoneError;
+    JLabel nomeError;
 
     public CadastroClienteView() {
         this.setViewLayout();
@@ -52,11 +55,15 @@ public class CadastroClienteView extends JPanel {
         fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.Y_AXIS));
 
         JPanel nomeFieldPanel = new JPanel();
-        JLabel nomeLabel = new JLabel("Nome:");
+        JLabel nomeLabel = new JLabel("Nome completo:");
         nomeFieldPanel.add(nomeLabel);
         this.nomeField = new JTextField(1);
         this.nomeField.setColumns(20);
         nomeFieldPanel.add(this.nomeField);
+        this.nomeError = new JLabel("Insira o nome completo do paciente");
+        this.nomeError.setForeground(Color.red);
+        this.nomeError.setVisible(false);
+        nomeFieldPanel.add(this.nomeError);
         fieldsPanel.add(nomeFieldPanel);
 
         JPanel cpfFieldPanel = new JPanel();
@@ -65,6 +72,10 @@ public class CadastroClienteView extends JPanel {
         this.cpfField = new JTextField(1);
         this.cpfField.setColumns(20);
         cpfFieldPanel.add(this.cpfField);
+        this.cpfError = new JLabel("Insira um CPF válido no formato XXX.XXX.XXX-XX");
+        this.cpfError.setForeground(Color.red);
+        this.cpfError.setVisible(false);
+        cpfFieldPanel.add(this.cpfError);
         fieldsPanel.add(cpfFieldPanel);
 
         JPanel telefoneFieldPanel = new JPanel();
@@ -73,6 +84,10 @@ public class CadastroClienteView extends JPanel {
         this.telefoneField = new JTextField(1);
         this.telefoneField.setColumns(20);
         telefoneFieldPanel.add(this.telefoneField);
+        this.telefoneError = new JLabel("Insira o telefone no formato (XX) XXXXX-XXXX");
+        this.telefoneError.setForeground(Color.red);
+        this.telefoneError.setVisible(false);
+        telefoneFieldPanel.add(this.telefoneError);
         fieldsPanel.add(telefoneFieldPanel);
 
         this.add(fieldsPanel, BorderLayout.CENTER);
@@ -91,15 +106,57 @@ public class CadastroClienteView extends JPanel {
         ClientesController clientesController = new ClientesController();
         JButton cadastrarButton = new JButton("Novo");
         cadastrarButton.addActionListener((ActionEvent e) -> {
-            clientesController.criarCliente(
-                this.nomeField.getText(),
-                this.cpfField.getText(),
-                this.telefoneField.getText()
-            );
-            Router.getInstance().goToView(new ClientesView());
+            if (this.validateFields()) {
+                clientesController.criarCliente(
+                    this.nomeField.getText(),
+                    this.cpfField.getText(),
+                    this.telefoneField.getText()
+                );
+                Router.getInstance().goToView(new ClientesView());
+            }
         });
         rodapePanel.add(cadastrarButton);
 
         this.add(rodapePanel, BorderLayout.SOUTH);
+    }
+    
+    private boolean validateFields() {
+        return this.validateNomeField() & this.validateCpfField() & this.validateTelefoneField();
+    }
+
+    private boolean validateNomeField() {
+        String nome = this.nomeField.getText();
+
+        if (nome.length() > 0) { 
+            this.nomeError.setVisible(false);
+            return true;
+        }
+
+        this.nomeError.setVisible(true);
+        return false;
+    }
+    
+    private boolean validateCpfField() {
+        String cpf = this.cpfField.getText();
+
+        if (cpf.matches("[\\d]{3}[\\.][\\d]{3}[\\.][\\d]{3}[\\-][\\d]{2}")) { 
+            this.cpfError.setVisible(false);
+            return true;
+        }
+
+        this.cpfError.setVisible(true);
+        return false;
+    }
+    
+    private boolean validateTelefoneField() {
+        String telefone = this.telefoneField.getText();
+
+        if (telefone.matches("[\\(][\\d]{2}[\\)][\\s][\\d]{5}[\\-][\\d]{4}")) { 
+            this.telefoneError.setVisible(false);
+            return true;
+        }
+
+        this.telefoneError.setVisible(true);
+        return false;
     }
 }
